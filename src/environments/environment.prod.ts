b65e632e2ae61e33c7be73f6815816ddbe0e7633
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-26 13:19:02 +0530

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  //prod
+  apiUrl: 'https://customerdigitalconnect.com',
+  //apiUrl: 'http://13.53.130.198:8080', //Uat
+
+  EncryptKey:"1203199320052021",
+  SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/chat',
+  googleMapsApiKey: 'YOUR_API_KEY',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://customerdigitalconnect.com',
+  // apiUrl: 'http://13.53.130.198:8080', //Uat
+  EncryptKey: '1203199320052021',
+  SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/chat',
+  googleMapsApiKey: 'YOUR_API_KEY',
+};
