e3147ccb851ab6d9761a927ec82e8647d5934668
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-05-02 15:05:19 +0530

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'customerdigitalconnect.com', //Uat
-  SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/v2.0/chat',
+//   apiUrl: 'customerdigitalconnect.com', //Uat
+//   SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/v2.0/chat',
 
   EncryptKey: '1203199320052021',
-  //   apiUrl: 'https://tcplconnecto.com/v2.0',
-//   SOCKET_ENDPOINT: 'wss://tcplconnecto.com/v2.0/chat',
+    apiUrl: 'https://tcplconnecto.com/v2.0',
+  SOCKET_ENDPOINT: 'wss://tcplconnecto.com/v2.0/chat',
   googleMapsApiKey: 'YOUR_API_KEY',
 };
