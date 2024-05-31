747ec99059ff2708b6198000bc62fe913a831f03
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-05-31 18:01:51 +0530

    Update

--- src/app/modules/activecustomerwithroomnumber/activecustomerwithroomnumber.component.ts
+++ src/app/modules/activecustomerwithroomnumber/activecustomerwithroomnumber.component.ts
@@ -9,118 +9,139 @@ import { ApiService } from 'src/app/_api/rxjs/api.service';
 import { AppService } from 'src/app/_services/app.service';
 
 @Component({
-  selector: 'app-activecustomerwithroomnumber',
-  templateUrl: './activecustomerwithroomnumber.component.html',
-  styleUrls: ['./activecustomerwithroomnumber.component.css']
+    selector: 'app-activecustomerwithroomnumber',
+    templateUrl: './activecustomerwithroomnumber.component.html',
+    styleUrls: ['./activecustomerwithroomnumber.component.css']
 })
 export class ActivecustomerwithroomnumberComponent {
-  isProceess: boolean = true;
-  data: any[] = [];
-  subscription?: Subscription;
-  userData: any;
-  masterName?: any;
-  selectedValue?: any = 7;
-  startDate?: any;
-  endDate?: any;
-  dateRangeError: boolean = false;
-  totalRecord: any;
-  term: any;
-  page: number = 1;
-  count: number = 0;
-  tableSize: number = 10;
-  tableSizes: any = [3, 6, 9, 12];
-  constructor(
-    private cd: ChangeDetectorRef,
-    private modalService: NgbModal,
-    private toastr: ToastrService,
-    private titleService: Title,
-    private appService: AppService,
-    private apiService: ApiService,
-    private datePipe: DatePipe,
-    private router: Router,
+    isProceess: boolean = true;
+    data: any[] = [];
+    subscription?: Subscription;
+    userData: any;
+    masterName?: any;
+    selectedValue?: any = 7;
+    startDate?: any;
+    endDate?: any;
+    dateRangeError: boolean = false;
+    totalRecord: any;
+    term: any;
+    page: number = 1;
+    count: number = 0;
+    tableSize: number = 10;
+    tableSizes: any = [3, 6, 9, 12];
+    constructor(
+        private cd: ChangeDetectorRef,
+        private modalService: NgbModal,
+        private toastr: ToastrService,
+        private titleService: Title,
+        private appService: AppService,
+        private apiService: ApiService,
+        private datePipe: DatePipe,
+        private router: Router,
 
-  ) {
-    this.titleService.setTitle('CDC -Chat histroy Report');
-    const d: any = localStorage.getItem('userData');
-    this.userData = JSON.parse(d);
+    ) {
+        this.titleService.setTitle('CDC -Chat histroy Report');
+        const d: any = localStorage.getItem('userData');
+        this.userData = JSON.parse(d);
 
-    const oneWeekFromNow = new Date();
-    this.endDate = this.datePipe.transform(
-      oneWeekFromNow.toISOString().split('T')[0],
-      'yyyy-MM-dd'
-    );
-    oneWeekFromNow.setDate(oneWeekFromNow.getDate() - 7);
-    this.startDate = this.datePipe.transform(
-      oneWeekFromNow.toISOString().split('T')[0],
-      'yyyy-MM-dd'
-    );
-  }
+        const oneWeekFromNow = new Date();
+        this.endDate = this.datePipe.transform(
+            oneWeekFromNow.toISOString().split('T')[0],
+            'yyyy-MM-dd'
+        );
+        oneWeekFromNow.setDate(oneWeekFromNow.getDate() - 7);
+        this.startDate = this.datePipe.transform(
+            oneWeekFromNow.toISOString().split('T')[0],
+            'yyyy-MM-dd'
+        );
+    }
 
-  ngOnInit(): void {
-    this.fatchData();
+    ngOnInit(): void {
+        this.fatchData();
 
-  }
+    }
 
-  calculateIndex(page: number, index: number): number {
-    return (page - 1) * this.tableSize + index + 1;
-}
+    calculateIndex(page: number, index: number): number {
+        return (page - 1) * this.tableSize + index + 1;
+    }
 
-  fatchData() {
-    this.isProceess = true;
-
-
-    this.masterName = `/customer/activecustomerwithroomnumber`;
-    this.subscription = this.apiService
-      .getAll(this.masterName)
-      .pipe(take(1))
-      .subscribe(
-        (data) => {
-          if (data) {
-            this.data = data.data;
-            this.count = this.data.length;
-            this.isProceess = false;
-            this.cd.detectChanges();
-
-          }
-        },
-        (error) => {
-          this.isProceess = false;
-        }
-      );
-  }
-
-  onTableDataChange(event: any) {
-    this.page = event;
-  }
-  onTableSizeChange(event: any): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-  }
-
-  trackByFn(index: number, item: any): number {
-    return item.categoryId;
-  }
+    fatchData() {
+        this.isProceess = true;
 
 
-  onDownload() {
-    const exportData = this.data.map((x) => {
-      return {
-        'guestName': x.guestName || '',
-        'guestNumber': x.guestNumber || '',
-        'guestCheckedInTime': x?.guestCheckedInTime || '',
-        'roomNumber': x?.roomNumber || '',
+        this.masterName = `/customer/activecustomerwithroomnumber`;
+        this.subscription = this.apiService
+            .getAll(this.masterName)
+            .pipe(take(1))
+            .subscribe(
+                (data) => {
+                    if (data) {
+                        this.data = data.data;
+                        this.count = this.data.length;
+                        this.isProceess = false;
+                        this.cd.detectChanges();
 
-      };
-    });
-    const headers = [
-      'guestName',
-      'guestNumber',
-      'guestCheckedInTime',
-      'roomNumber'
+                    }
+                },
+                (error) => {
+                    this.isProceess = false;
+                }
+            );
+    }
 
-    ];
-    this.appService.exportAsExcelFile(exportData, ' Active Customer Roomnumber', headers);
-  }
+    onCatlog() {
+
+        this.masterName = `/campaign`;
+        this.subscription = this.apiService.add(this.masterName)
+            .pipe(take(1))
+            .subscribe(
+                (data) => {
+                    if (data) {
+                        this.toastr.success("");
+                        this.isProceess = false;
+                        this.cd.detectChanges();
+
+                    }
+                },
+                (error) => {
+                    this.isProceess = false;
+                }
+            );
+
+    }
+
+    onTableDataChange(event: any) {
+        this.page = event;
+    }
+    onTableSizeChange(event: any): void {
+        this.tableSize = event.target.value;
+        this.page = 1;
+    }
+
+    trackByFn(index: number, item: any): number {
+        return item.categoryId;
+    }
+
+
+    onDownload() {
+        const exportData = this.data.map((x) => {
+            return {
+                'guestName': x.guestName || '',
+                'guestNumber': x.guestNumber || '',
+                'guestCheckedInTime': x?.guestCheckedInTime || '',
+                'roomNumber': x?.roomNumber || '',
+
+            };
+        });
+        const headers = [
+            'guestName',
+            'guestNumber',
+            'guestCheckedInTime',
+            'roomNumber'
+
+        ];
+        this.appService.exportAsExcelFile(exportData, ' Active Customer Roomnumber', headers);
+    }
 
 
 
