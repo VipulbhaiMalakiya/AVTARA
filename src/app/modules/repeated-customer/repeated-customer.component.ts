aefbce17552a9ba176a59c6af08aea8a44bd8366
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-06-26 19:29:16 +0530

    Repeated Customer

--- src/app/modules/repeated-customer/repeated-customer.component.ts
+++ src/app/modules/repeated-customer/repeated-customer.component.ts
@@ -9,118 +9,145 @@ import { ApiService } from 'src/app/_api/rxjs/api.service';
 import { AppService } from 'src/app/_services/app.service';
 
 @Component({
-  selector: 'app-repeated-customer',
-  templateUrl: './repeated-customer.component.html',
-  styleUrls: ['./repeated-customer.component.css']
+    selector: 'app-repeated-customer',
+    templateUrl: './repeated-customer.component.html',
+    styleUrls: ['./repeated-customer.component.css']
 })
 export class RepeatedCustomerComponent {
-  isProceess: boolean = true;
-  data: any[] = [];
-  subscription?: Subscription;
-  userData: any;
-  masterName?: any;
-  selectedValue?: any = 7;
-  startDate?: any;
-  endDate?: any;
-  dateRangeError: boolean = false;
-  totalRecord: any;
-  term: any;
-  page: number = 1;
-  count: number = 0;
-  tableSize: number = 10;
-  tableSizes: any = [3, 6, 9, 12];
-  constructor(
-    private cd: ChangeDetectorRef,
-    private modalService: NgbModal,
-    private toastr: ToastrService,
-    private titleService: Title,
-    private appService: AppService,
-    private apiService: ApiService,
-    private datePipe: DatePipe,
-    private router: Router,
+    isProceess: boolean = true;
+    data: any[] = [];
+    customerData: any[] = [];
+    subscription?: Subscription;
+    userData: any;
+    masterName?: any;
+    selectedValue?: any = 7;
+    startDate?: any;
+    endDate?: any;
+    dateRangeError: boolean = false;
+    totalRecord: any;
+    term: any;
+    page: number = 1;
+    count: number = 0;
+    tableSize: number = 10;
+    tableSizes: any = [3, 6, 9, 12];
+    constructor(
+        private cd: ChangeDetectorRef,
+        private modalService: NgbModal,
+        private toastr: ToastrService,
+        private titleService: Title,
+        private appService: AppService,
+        private apiService: ApiService,
+        private datePipe: DatePipe,
+        private router: Router,
 
-  ) {
-    this.titleService.setTitle('CDC -Chat histroy Report');
-    const d: any = localStorage.getItem('userData');
-    this.userData = JSON.parse(d);
+    ) {
+        this.titleService.setTitle('CDC -Chat histroy Report');
+        const d: any = localStorage.getItem('userData');
+        this.userData = JSON.parse(d);
 
-    const oneWeekFromNow = new Date();
-    this.endDate = this.datePipe.transform(
-      oneWeekFromNow.toISOString().split('T')[0],
-      'yyyy-MM-dd'
-    );
-    oneWeekFromNow.setDate(oneWeekFromNow.getDate() - 7);
-    this.startDate = this.datePipe.transform(
-      oneWeekFromNow.toISOString().split('T')[0],
-      'yyyy-MM-dd'
-    );
-  }
+        const oneWeekFromNow = new Date();
+        this.endDate = this.datePipe.transform(
+            oneWeekFromNow.toISOString().split('T')[0],
+            'yyyy-MM-dd'
+        );
+        oneWeekFromNow.setDate(oneWeekFromNow.getDate() - 7);
+        this.startDate = this.datePipe.transform(
+            oneWeekFromNow.toISOString().split('T')[0],
+            'yyyy-MM-dd'
+        );
+    }
 
-  ngOnInit(): void {
-    this.fatchData();
+    ngOnInit(): void {
+        this.fatchData();
+        this.fatchCustomerData();
 
-  }
+    }
 
-  calculateIndex(page: number, index: number): number {
-    return (page - 1) * this.tableSize + index + 1;
-}
-
-  fatchData() {
-    this.isProceess = true;
-
-
-    this.masterName = `/customer/repeatedCustomer`;
-    this.subscription = this.apiService
-      .getAll(this.masterName)
-      .pipe(take(1))
-      .subscribe(
-        (data) => {
-          if (data) {
-            this.data = data.data;
-            this.count = this.data.length;
+    fatchCustomerData() {
+        this.masterName = "/customer";
+        this.subscription = this.apiService.getAll(this.masterName).pipe(take(1)).subscribe(data => {
+            if (data) {
+                this.customerData = data;
+                this.cd.detectChanges();
+            }
+        }, error => {
             this.isProceess = false;
-            this.cd.detectChanges();
+        })
+    }
 
-          }
-        },
-        (error) => {
-          this.isProceess = false;
-        }
-      );
-  }
+    sendMessage(dataItem: any) {
 
-  onTableDataChange(event: any) {
-    this.page = event;
-  }
-  onTableSizeChange(event: any): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-  }
+        // Filter customers by a specific contact
+        const filteredCustomers = this.customerData.filter(customer => customer.contact === dataItem.mobilenumber);
 
-  trackByFn(index: number, item: any): number {
-    return item.categoryId;
-  }
+        // Log the filtered customers
+        const customerId = [filteredCustomers[0].customerId]
+
+        this.router.navigate([`/admin/inbox/${customerId}`]);
+        // window.location.href = `/admin/inbox/${dataItem.customerId}`
+    }
 
 
-  onDownload() {
-    const exportData = this.data.map((x) => {
-      return {
-        'Customer': x.customerName || '',
-        'Phone': x.mobilenumber || '',
-        'lastCheckedInDateTime': x?.lastCheckedInDateTime ? formatDate(x.lastCheckedInDateTime, 'medium', 'en-IN', 'IST') : '',
-        'lastCheckedoutDateTime': x?.lastCheckedoutDateTime ? formatDate(x.lastCheckedoutDateTime, 'medium', 'en-IN', 'IST') : '',
+    calculateIndex(page: number, index: number): number {
+        return (page - 1) * this.tableSize + index + 1;
+    }
 
-      };
-    });
-    const headers = [
-      'Customer',
-      'Phone',
-      'lastCheckedInDateTime',
-      'lastCheckedoutDateTime'
+    fatchData() {
+        this.isProceess = true;
 
-    ];
-    this.appService.exportAsExcelFile(exportData, ' repeatedCustomer', headers);
-  }
+
+        this.masterName = `/customer/repeatedCustomer`;
+        this.subscription = this.apiService
+            .getAll(this.masterName)
+            .pipe(take(1))
+            .subscribe(
+                (data) => {
+                    if (data) {
+                        this.data = data.data;
+                        this.count = this.data.length;
+                        this.isProceess = false;
+                        this.cd.detectChanges();
+
+                    }
+                },
+                (error) => {
+                    this.isProceess = false;
+                }
+            );
+    }
+
+    onTableDataChange(event: any) {
+        this.page = event;
+    }
+    onTableSizeChange(event: any): void {
+        this.tableSize = event.target.value;
+        this.page = 1;
+    }
+
+    trackByFn(index: number, item: any): number {
+        return item.categoryId;
+    }
+
+
+    onDownload() {
+        const exportData = this.data.map((x) => {
+            return {
+                'Customer': x.customerName || '',
+                'Phone': x.mobilenumber || '',
+                'lastCheckedInDateTime': x?.lastCheckedInDateTime ? formatDate(x.lastCheckedInDateTime, 'medium', 'en-IN', 'IST') : '',
+                'lastCheckedoutDateTime': x?.lastCheckedoutDateTime ? formatDate(x.lastCheckedoutDateTime, 'medium', 'en-IN', 'IST') : '',
+
+            };
+        });
+        const headers = [
+            'Customer',
+            'Phone',
+            'lastCheckedInDateTime',
+            'lastCheckedoutDateTime'
+
+        ];
+        this.appService.exportAsExcelFile(exportData, ' repeatedCustomer', headers);
+    }
 
 
 
