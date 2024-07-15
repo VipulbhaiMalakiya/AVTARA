691c937db30ea6068bf1e430dfac80724f7e9ac1
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-07-15 14:23:26 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: false,
-  //prod
-  apiUrl: 'https://tcplconnecto.com/v2.0/',
-  //apiUrl: 'http://13.53.130.198:8080', //Uat
+    production: false,
+    //prod
+    apiUrl: 'https://tcplconnecto.com/v2.0',
+    //apiUrl: 'http://13.53.130.198:8080', //Uat
 
-  EncryptKey:"1203199320052021",
-  SOCKET_ENDPOINT: 'wss://tcplconnecto.com/chat',
-  googleMapsApiKey: 'YOUR_API_KEY',
+    EncryptKey: "1203199320052021",
+    SOCKET_ENDPOINT: 'wss://tcplconnecto.com/chat',
+    googleMapsApiKey: 'YOUR_API_KEY',
 };
