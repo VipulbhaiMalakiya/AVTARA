a25e22884c323ef0ddb665b18ed46efdd13d0d4a
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-08-12 10:01:56 +0530

    update

--- src/app/modules/2024/order-list/components/order-update/order-update.component.ts
+++ src/app/modules/2024/order-list/components/order-update/order-update.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../../../Model/oder-model';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from '../../../Service/order.service';
 
 @Component({
-  selector: 'app-order-update',
-  templateUrl: './order-update.component.html',
-  styleUrls: ['./order-update.component.css']
+    selector: 'app-order-update',
+    templateUrl: './order-update.component.html',
+    styleUrls: ['./order-update.component.css']
 })
-export class OrderUpdateComponent {
+export class OrderUpdateComponent implements OnInit {
+    isProceess: boolean = true;
+    data: Order[] = [];
+    order: Order | undefined;
 
+    constructor(private route: ActivatedRoute, private orderService: OrderService) { }
+
+    ngOnInit(): void {
+        this.route.paramMap.subscribe(params => {
+            const id = params.get('id');
+            if (id) {
+                this.loadOrders(id);
+            }
+        });
+    }
+
+    loadOrders(id: any): void {
+        this.orderService.getOrderById(id).subscribe(order => {
+            this.order = order;
+            console.log(this.order);
+
+        });
+    }
+
+    steps = [
+        { status: 'Confirmation', icon: 'las la-check-circle' },
+        { status: 'Sales', icon: 'las la-user-tie' },
+        { status: 'In Transit', icon: 'las la-shuttle-van' },
+        { status: 'Delivery', icon: 'las la-thumbs-up' }
+    ];
+    getStepClass(status: string): string {
+        const trackStatus = this.order?.trackStatus;
+        const steps = this.steps ?? [];
+
+        if (trackStatus === status) {
+            return 'active'; // Current step
+        } else if (steps.length > 0) {
+            const currentStepIndex = steps.findIndex(step => step.status === trackStatus);
+            const targetStepIndex = steps.findIndex(step => step.status === status);
+
+            if (currentStepIndex !== -1 && targetStepIndex !== -1) {
+                // Mark steps before the current step as 'finish'
+                if (targetStepIndex < currentStepIndex) {
+                    return 'finish';
+                }
+                // Mark steps after the current step as 'inactive'
+                else if (targetStepIndex > currentStepIndex) {
+                    return 'inactive';
+                }
+            }
+        }
+        return 'inactive'; // Default for steps that don't match
+    }
 }
